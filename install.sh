ad3d6195e7fe6962a61bf5904d5ac6a8c0143a1b
Praney Behl <praneybehl@gmail.com>
2016-03-26 10:53:26 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 SCRIPTS_PATH="$HOME/Library/Scripts/Folder Action Scripts"
 
 mkdir -p "$SCRIPTS_PATH"
-curl -sSO https://raw.githubusercontent.com/svg/svgo-osx-folder-action/master/svgo.applescript
-osacompile -o "$SCRIPTS_PATH/svgo.scpt" svgo.applescript
-rm svgo.applescript
+curl -sSO https://raw.githubusercontent.com/praneybehl/svgo-osx-folder-action/master/svgo_pretty.applescript
+osacompile -o "$SCRIPTS_PATH/svgo_pretty.scpt" svgo_pretty.applescript
+rm svgo_pretty.applescript
